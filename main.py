56eed583decdc5bcdb1150ffbeb2ecb0bac7748d
Mateo Rojas <mateorojasarce2003@gmail.com>
2025-11-09 00:21:44 -0300

    cambios

--- main.py
+++ main.py
@@ -121,4 +121,5 @@ def guardar_tasa_individual(banco, tna):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+     port = int(os.environ.get("PORT", 5000))
+     app.run(host="0.0.0.0", port=port)
